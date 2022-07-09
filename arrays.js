0e577f2b554c6340acb3a1b49ff9631631f05a73
Lauren Frazier <lauren.e.frazier@gmail.com>
2022-07-09 08:41:21 -0600

    Add js comments to array methods

--- arrays.js
+++ arrays.js
@@ -1,8 +1,15 @@
 var collegeFriends = ["Samantha", "Angelica", "Beth", "Evelyn", "Raquel"];
+collegeFriends.push("Emma", "Rachel");
 console.log(collegeFriends);
+// this will add "Emma" and "Rachel" to the array
 
 var mileLog = [4, 6, 3, 2, 7];
+mileLog.pop();
 console.log(mileLog);
+// this will remove the last element from the array
+
 
 var testAnswers = [true, true, false, true];
-console.log(testAnswers); 
+testAnswers.shift();
+console.log(testAnswers);
+// this will remove the first element in the array
