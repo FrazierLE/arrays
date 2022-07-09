5746046ef992b2335102afdd1aca194c14656a38
Lauren Frazier <lauren.e.frazier@gmail.com>
2022-07-09 08:28:42 -0600

    Add variables

---
+++ arrays.js
@@ -0,0 +1,8 @@
+var collegeFriends = ["Samantha", "Angelica", "Beth", "Evelyn", "Raquel"];
+console.log(collegeFriends);
+
+var mileLog = [4, 6, 3, 2, 7];
+console.log(mileLog);
+
+var testAnswers = [true, true, false, true];
+console.log(testAnswers); 
